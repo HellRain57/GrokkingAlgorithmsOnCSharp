66a0e99191da26838328a7936dfdd32664390859
HellRain57 <109213391+HellRain57@users.noreply.github.com>
2022-07-14 11:02:46 +0300

    Binary Search / Бинарный поиск

---
+++ BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] a = { 1, 3, 5, 7, 9 };
+        Console.WriteLine(Convert.ToString(BinarySearch(a, 5)));
+    }
+
+    
+    //==================== Бинарный поиск ====================//
+    static int BinarySearch(int[] inputArray, int searchNumber)
+    {
+        int low = 0;
+        int high = inputArray.Length-1;
+
+        while (low <= high)
+        {            
+            int mid = (high + low) / 2;
+            if (searchNumber < inputArray[mid])
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+
+            if (inputArray[mid] == searchNumber)
+            {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+    //========================================================//
+}
