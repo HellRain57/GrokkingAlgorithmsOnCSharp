e8bb3ca779459d0c3d4cd15f81a1e7ec7c345ca7
HellRain57 <109213391+HellRain57@users.noreply.github.com>
2022-07-14 10:54:17 +0300

    Selection sort / Сортировка выбором

--- Program.cs
+++ Program.cs
@@ -1,37 +1,69 @@
-﻿class Program
+﻿class Program 
 {
-    static void Main(string[] args)
+    static void Main(string[] args) 
     {
-        int[] a = { 1, 3, 5, 7, 9 };
-        Console.WriteLine(Convert.ToString(BinarySearch(a, 5)));
+        var inputList = new List<int>();
+        inputList = RandList(inputList, 10);
+
+        foreach (var item in inputList)
+        {
+            Console.Write(item + " | ");
+        }
+        Console.WriteLine();
+
+
+        var outputList = selectionSort(inputList);
+
+        Console.WriteLine("Sorted:");
+        foreach (var item in outputList)
+        {
+            Console.Write(item + " | ");
+        }
     }
 
-    
-    //==================== Бинарный поиск ====================//
-    static int BinarySearch(int[] inputArray, int searchNumber)
+
+    //================= Рандом массива =================//
+    private static new List<int> RandList(List<int> inputList, int numOfElements) 
     {
-        int low = 0;
-        int high = inputArray.Length-1;
+        for (int i = 0; i < numOfElements; i++) 
+        {
+            Random rnd = new Random();
+            inputList.Add(rnd.Next(0, 9));
+        }
+        return inputList;
+    }
+    //==================================================//
 
-        while (low <= high)
-        {            
-            int mid = (high + low) / 2;
-            if (searchNumber < inputArray[mid])
-            {
-                high = mid-1;
-            }
-            else
-            {
-                low = mid+1;
-            }
+    //=============== Сортировка выбором ===============//
+    public static new List<int> selectionSort(List<int> inputList)
+    {
+        var outputList = new List<int>();
+        int inputListLenght = inputList.Count;
+        for (int i = 0; i < inputListLenght; i++)
+        { 
+            int smallest = findSmallest(inputList);
+            outputList.Add(inputList[smallest]);
+            inputList.RemoveAt(smallest);            
+        }
+        return outputList;
+    }
+    //==================================================//
 
-            if (inputArray[mid] == searchNumber)
+    //=============== Поиск минимального ===============//
+    private static int findSmallest(List<int> inputList)
+    {
+        int smallest = inputList[0];
+        int smallestIndex = 0;
+
+        for (int i = 0; i < inputList.Count; i++)
+        {
+            if (inputList[i] < smallest) 
             {
-                return mid;
+                smallest = inputList[i];
+                smallestIndex = i;
             }
         }
-
-        return -1;
+        return smallestIndex;
     }
-    //========================================================//
+    //==================================================//
 }
