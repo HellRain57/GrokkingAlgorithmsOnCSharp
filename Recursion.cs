f07e84caecebee2b8c0ad910a71b0084215fcb10
HellRain57 <109213391+HellRain57@users.noreply.github.com>
2022-10-12 10:44:02 +0300

    Recursion / Рекурсия

---
+++ Recursion.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {        
+        //======================= Факториал =======================//
+        Console.WriteLine("Введите число для расчета факториала: ");
+        int i = Convert.ToInt32(Console.ReadLine());
+        int res = Factorial(i);
+        Console.WriteLine("Результат: " + res);
+        //=========================================================//
+
+        //======================= Факториал =======================//
+        Console.WriteLine("Введите число для расчета n-ого члена Фибоначчи: ");
+        i = Convert.ToInt32(Console.ReadLine());
+        res = Fibonachchi(i);
+        Console.WriteLine("Результат: " + res);
+    }
+
+    public static int Factorial(int i)
+    {
+        if (i == 1) return i;
+        else return i * (Factorial(i - 1));
+    }
+
+    public static int Fibonachchi(int i)
+    {
+        if ((i == 1) || (i == 0)) return i;
+        else return Fibonachchi(i - 1) + Fibonachchi(i - 2);
+    }
+}
